2ce9b3721169ad115d0fb6056214dabdeaba62c3
haidy <haidygone@gmail.com>
2024-07-20 13:48:38 +0800

    update build script

--- cmd/frp/build_android.sh
+++ cmd/frp/build_android.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+FRP_VERSION=$1
+
 unset GO111MODULE
 unset GOMOD
+
+if [[ -d libfrp ]]; then
+  rm -rf libfrp
+fi
+
+if [[ -f libfrp.aar ]]; then
+  rm -v libfrp.aar
+fi
+
+if [[ -f libfrp-sources.jar ]]; then
+  rm -v libfrp-sources.jar
+fi
 
 if [ ! -d "${ANDROID_HOME}/ndk-bundle" ] && [ ! -d "${ANDROID_NDK_HOME}" ]; then
   if [ "Darwin" == "$(uname)" ]; then
@@ -14,7 +28,12 @@ fi
 go env -w GO111MODULE=auto
 gomobile bind -v -o libfrp.aar -target=android .
 
-if [[ ! -d libfrp ]]; then
-  rm -rf libfrp
+unzip -d libfrp libfrp.aar
+
+if [[ ! -z $FRP_VERSION ]]; then
+  cp -v -rf libfrp/jni libfrp/jniV
+  mv libfrp/jniV/arm64-v8a/libgojni.so libfrp/jniV/arm64-v8a/libgojni.${FRP_VERSION}.so
+  mv libfrp/jniV/armeabi-v7a/libgojni.so libfrp/jniV/armeabi-v7a/libgojni.${FRP_VERSION}.so
+  mv libfrp/jniV/x86/libgojni.so libfrp/jniV/x86/libgojni.${FRP_VERSION}.so
+  mv libfrp/jniV/x86_64/libgojni.so libfrp/jniV/x86_64/libgojni.${FRP_VERSION}.so
 fi
-unzip -d libfrp libfrp.aar 
